59c5657b4708f07e6463ba61ee7b7c4707e93044
varun5299 <varun.agarwal5299@gmail.com>
2020-12-01 19:59:08 +0530

    Initialized Project.

---
+++ src/main/java/com/practice/ChuckNorrisJokeApp/ChuckNorrisJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.practice.ChuckNorrisJokeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/ChuckNorrisJokeApp/Norris/Joke/App/ChuckNorrisJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.ChuckNorrisJokeApp.Norris.Joke.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
